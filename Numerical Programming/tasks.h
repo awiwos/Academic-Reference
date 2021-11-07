e48fa8dc9f470cb109481963cc2f3cffa4ecf24a
awiwos <92239335+awiwos@users.noreply.github.com>
2021-11-07 22:38:31 +1100

    Add files via upload

---
+++ Numerical Programming/tasks.h
@@ -0,0 +1,13 @@
+
+
+#ifndef TASKS_H
+
+void maxfluxdiff(const char* flow_file);
+
+void coarsegrid(const char* flow_file, int resolution);
+
+void searching(const char* flow_file);
+
+void vortcalc(const char* flow_file);
+
+#endif
